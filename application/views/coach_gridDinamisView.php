defc70dfad8afd103243a52c72ee117d8218be01
admkrnia <admkrnia@users.noreply.github.com>
2018-07-19 16:02:37 +0700

    view grid

---
+++ application/views/coach_gridDinamisView.php
@@ -0,0 +1,20 @@
+<?php 
+        $this->load->view('header');
+?>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url()?>assets/jsgrid/jsgrid.min.css" />
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url()?>assets/jsgrid/jsgrid-theme.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="<?php echo base_url()?>assets/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>assets/jsgrid/jsgrid.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>assets/custom/coach_grid.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div id="jsGrid"></div>
+    </div>
+</body>
